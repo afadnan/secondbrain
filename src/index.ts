66d2241665f69ec08cf335ded367e75d74b8eb94
Adnan Ashraf <af.adnan23@gmail.com>
2024-12-08 14:17:39 +0530

    user signup Done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
-import express from "express";
+import express,{Request,Response} from "express";
 import mongoose from "mongoose";
-
-
+import { z } from "zod";
+import { userZodSchema } from "./validation";
+import { UserModel } from "./db";
+import bcrypt from "bcrypt";
 dotenv.config();
-// console.log("MONGO_URL:", process.env.MONGO_URL);
+
+
+
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 4040;
 const MONGO_URL= process.env.MONGO_URL || "";
 if(!MONGO_URL){
@@ -17,7 +22,7 @@ async function main() {
     await mongoose.connect(MONGO_URL);
     console.log("Connected to DB");
     app.listen(PORT, () => {
-      console.log(`Server is Running on http://${PORT}`);
+      console.log(`Server is Running on http://localhost:${PORT}`);
     });
   } catch (e) {
     console.error("DB is not connected ");
@@ -26,10 +31,40 @@ async function main() {
   }
 }
 main();
+//@ts-ignore
+app.post("/api/v1/signup",async function (req:Request,res:Response){
+  try {
+    const validateUser = userZodSchema.parse(req.body);
+    const existingUser = await UserModel.findOne({email : validateUser.email})
+    if(existingUser){
+      return res.status(400).json({message:"This email alredy exists",email:validateUser.email})
+    }
+    const passwordHash = await bcrypt.hash(validateUser.password,5)
+    const newUser = await UserModel.create({
+      email:validateUser.email,
+      password:passwordHash,
+    })
+    res.status(201).json({message:"user signup successfull",email:newUser.email})
+  
+  } catch (error:any) {
+    // Handle Zod validation errors
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        message: "Zod Validation Incorrect",
+        errors: error.errors,
+      });
+    }
 
-app.post("/api/v1/signup", function (req, res) {
-  const { email, password } = req.body;
+    // Handle all other errors
+    console.error("Internal Server Error:", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message || error,
+    });
+  }
 });
+
+
 
 app.post("/api/v1/signin", function (req, res) {
   const { email, password } = req.body;
--- src/validation.ts
+++ src/validation.ts
@@ -4,6 +4,11 @@ export const userZodSchema = z.object({
     email:z.string().email("Invalid email formate").trim(),
     password:z.string()
     .min(7,"atlest more than 7 character")
-    .max(30,"not excessed 30 character")
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/,"one uppercase,one lowercase,one number,one special character is required")
+    .max(30,"not more than 30 character")
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/,"required atlest one uppercase,one lowercase,one number,one special character is required")
 })
+
+type UserZodSchema = z.infer<typeof userZodSchema>;
+export {
+    UserZodSchema
+}
