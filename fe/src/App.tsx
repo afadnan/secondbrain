d678dc754364c95f477fa89e26a37dcd03673d05
Adnan Ashraf <af.adnan23@gmail.com>
2024-12-29 22:51:40 +0530

    added twitter script && pass the props data

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
     <Button variant="primary" text="Add Content" startIcon={<PlusIcon />}></Button>
     <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}></Button>
+    <Card type="twitter" link="https://x.com/cute_catse/status/1869782853237797325" title="First Tweet"/>
     </div>
       )
 }
