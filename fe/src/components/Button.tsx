f4b5f604a597a2c3c10defe569183feac5e14ac6
Adnan Ashraf <af.adnan23@gmail.com>
2024-12-30 21:30:53 +0530

    Add Content open the modal and cross close the modal

--- fe/src/components/Button.tsx
+++ fe/src/components/Button.tsx
@@ -15,8 +15,8 @@ const variantClasses = {
 
 const defaultStyles = "px-4 py-2 rounded-md font-light flex justify-center items-center"
 
-export function Button({variant,text,startIcon}:ButtonProps){
-    return <button className={variantClasses[variant] + " " + defaultStyles}>
+export function Button({variant,text,startIcon,onClick}:ButtonProps){
+    return <button onClick={onClick} className={variantClasses[variant] + " " + defaultStyles}>
         <div className="pr-2">
             {startIcon}
         </div>
