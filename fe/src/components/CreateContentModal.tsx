7357bda1dbb0fa927f2465d5329c1353c4fd4d8d
Adnan Ashraf <af.adnan23@gmail.com>
2025-01-27 23:02:39 +0530

    Input Component

--- fe/src/components/CreateContentModal.tsx
+++ fe/src/components/CreateContentModal.tsx
@@ -28,8 +28,3 @@ export function CreateContentModal({open,onClose}:any){
      </div>
 }
 
-function Input ({onChange, placeholder}: {onChange: () => void}){
-    return <div>
-        <input placeholder={placeholder} type={"text"} className="px-4 py-2 border-2 rounded-md m-2 " onChange={onChange} ></input>
-    </div>
-}
