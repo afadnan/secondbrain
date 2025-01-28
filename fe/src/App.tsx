24a8e3dff85a3236f98fb0a5a6ddcbd574bb254a
Adnan Ashraf <af.adnan23@gmail.com>
2025-01-29 00:39:42 +0530

    Fixed Signin Component

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import Dashboard from "./pages/Dashboard"
 import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin"
 
 export default function App(){
-  return (<Signup/>) 
+  return (<Signin/>) 
 }
--- fe/src/pages/Signin.tsx
+++ fe/src/pages/Signin.tsx
@@ -1,14 +1,14 @@
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
-export function Signup() {
+export function Signin() {
     return (
         <div className="h-screen w-screen bg-gray-300 flex justify-center items-center">
             <div className="bg-white rounded-xl p-10 border min-w-48">
                 <Input placeholder="Username" />
                 <Input placeholder="Password" />
                 <div className="flex justify-center pt-4 ">
-                <Button variant="primary" text="Signup" fullWidth={true} loading={false} />
+                <Button variant="primary" text="Signin" fullWidth={true} loading={false} />
                 </div>
             </div>
 
