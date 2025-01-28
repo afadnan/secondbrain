848dfbb21c4c3d05c79252866d1e7360f7b99281
Adnan Ashraf <af.adnan23@gmail.com>
2025-01-29 00:52:17 +0530

    Routing

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,16 @@
 import Dashboard from "./pages/Dashboard"
 import { Signup } from "./pages/Signup"
 import { Signin } from "./pages/Signin"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 export default function App(){
-  return (<Signin/>) 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  ) 
 }
