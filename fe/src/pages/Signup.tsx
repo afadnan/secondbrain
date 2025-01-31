ac0df7cff08fb6bad4a03994697ed2cc32af987f
Adnan Ashraf <af.adnan23@gmail.com>
2025-01-31 23:22:00 +0530

    able to navigate signup to signin

--- fe/src/pages/Signup.tsx
+++ fe/src/pages/Signup.tsx
@@ -8,17 +8,20 @@ import { useNavigate } from "react-router-dom";
 export function Signup() {
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
 
     async function signup() {
         const username = usernameRef.current?.value || "";
         const password = passwordRef.current?.value || "";
+        const email = emailRef.current?.value || "";
 
-        console.log("Sending request:", { username, password });
+        console.log("Sending request:", { username,email, password });
 
         try {
             await axios.post(`${BACKEND_URL}/api/v1/signup`, {
                 userName:username,
+                email,
                 password
             });
             alert("You have signed up!");
@@ -32,6 +35,7 @@ export function Signup() {
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
         <div className="bg-white rounded-xl border min-w-48 p-8">
             <Input reference={usernameRef} placeholder="Username" />
+            <Input reference={emailRef} placeholder="email" />
             <Input reference={passwordRef} placeholder="Password" />
             <div className="flex justify-center pt-4">
                 <Button onClick={signup} loading={false} variant="primary" text="Signup" fullWidth={true} />
