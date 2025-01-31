bfe2e15b6014b3cd24635524c4aa6020536d1b2a
Adnan Ashraf <af.adnan23@gmail.com>
2025-02-01 00:15:58 +0530

    connected fe with be

--- fe/src/pages/Signin.tsx
+++ fe/src/pages/Signin.tsx
@@ -1,17 +1,45 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function Signin() {
-    return (
-        <div className="h-screen w-screen bg-gray-300 flex justify-center items-center">
-            <div className="bg-white rounded-xl p-10 border min-w-48">
-                <Input placeholder="Username" />
-                <Input placeholder="Password" />
-                <div className="flex justify-center pt-4 ">
-                <Button variant="primary" text="Signin" fullWidth={true} loading={false} />
-                </div>
-            </div>
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
 
+    async function signin() {
+        const username = usernameRef.current?.value || "";
+        const password = passwordRef.current?.value || "";
+        const email = emailRef.current?.value || "";
+
+        console.log("Sending request:", { username,email, password });
+
+        try {
+            await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+                userName:username,
+                email,
+                password
+            });
+            
+            navigate("/dashboard");
+        } catch (error :any) {
+            console.error("Signin failed:", error.response?.data || error.message);
+            alert("Signin failed! Check console.");
+        }
+    }
+
+    return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+        <div className="bg-white rounded-xl border min-w-48 p-8">
+            <Input reference={usernameRef} placeholder="Username" />
+            <Input reference={emailRef} placeholder="email" />
+            <Input reference={passwordRef} placeholder="Password" />
+            <div className="flex justify-center pt-4">
+                <Button onClick={signin} loading={false} variant="primary" text="Signin" fullWidth={true} />
+            </div>
         </div>
-    )
+    </div>
 }
