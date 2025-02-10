bdaa8da59e6da90c19d7433f112f7da60d729796
Adnan Ashraf <af.adnan23@gmail.com>
2025-02-11 00:52:54 +0530

    refres content after 10 seconds

--- fe/src/hooks/useContent.ts
+++ fe/src/hooks/useContent.ts
@@ -1,19 +1,31 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
-import axios from "axios";
 
-export function useContent(){
-    const [contents,setContents] = useState([]);
+export function useContent() {
+    const [contents, setContents] = useState([]);
 
-    useEffect(()=>{
-            axios.get(`${BACKEND_URL}/api/v1/content`,{
-                headers : {
-                    "Authorization": "Bearer " + localStorage.getItem("token")
-                }
-            }).then((response)=> {
-            setContents(response.data.content)
+    function refresh() {
+        axios.get(`${BACKEND_URL}/api/v1/content`, {
+            headers: {
+                "Authorization": localStorage.getItem("token")
+            }
         })
-    },[])
+            .then((response) => {
+                setContents(response.data.content)
+            })
+    }
 
-    return contents;
+    useEffect(() => {
+        refresh()
+        let interval = setInterval(() => {
+            refresh()
+        }, 10 * 1000)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
+    return {contents, refresh};
 }
